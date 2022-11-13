71afe2d90321e7450f0425275937554752bd6746
TongQuangThanh <tongquangthanh94@gmail.com>
2022-11-13 20:22:32 +0700

    deploy

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive --acl public-read ./www s3://myawsbucket-75139724085/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://myawsbucket-75139724085/
+aws s3 cp --recursive --acl public-read ./www s3://myudacity747325614833/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://myudacity747325614833/
